4de750c5835591f640da55c117101c422d1b8e3e
Lucien Zhang <lucien_zhang@hotmail.com>
2020-06-10 22:51:52 +0900

    mnist

--- ml/__init__.py
+++ ml/__init__.py
@@ -1,5 +1,9 @@
-from fastapi import FastAPI, Form
+from fastapi import FastAPI, File, UploadFile
 from starlette.middleware.cors import CORSMiddleware
+import requests
+from PIL import Image
+import numpy as np
+import json
 
 from ml.core.config import settings
 
@@ -16,8 +20,17 @@ if settings.BACKEND_CORS_ORIGINS:
 
 
 @app.post("/ml-api/mnist")
-async def mnist(*, img: bytes = Form(...)):
-    return {
-        'result': 1,
-        'prob': 0.96
-    }
+async def mnist(*, img: UploadFile = File(...)):
+    image = Image.open(img.file).resize((28, 28)).convert('L')
+    image = np.array(image, dtype='float32').reshape((-1, 28, 28, 1))
+    image /= 255.0
+    data = json.dumps({
+        "instances": image.tolist()
+    })
+    headers = {"content-type": "application/json"}
+    url = settings.TF_PREDICT_URL.format('mnist')
+    json_response = await requests.post(url, data=data, headers=headers)
+    predictions = np.array(json.loads(json_response.text)['predictions'])
+    result = np.argmax(predictions, axis=-1)[0].item()
+    prob = '{:.2%}'.format(np.max(predictions, axis=-1)[0].item())
+    return json.dumps(dict(result=result, prob=prob))
